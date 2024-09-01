d648894e9cf52a54a2794565fc0a1ea6b4cbbcb8
NicolasWillyam <phambahuyhoang0@gmail.com>
2024-09-01 17:07:42 +0700

    Implement toolbar

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -26,7 +26,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className=" px-3 py-2 w-full flex items-center justify-between">
+      <nav className="h-10 bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
@@ -41,7 +41,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   return (
     <>
-      <nav className=" px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="h-10 bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
           <MenuIcon
             role="button"
--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -68,7 +68,6 @@ export const TrashBox = () => {
   return (
     <div className="text-sm">
       <div className="flex items-center gap-x-1 p-2">
-        <Search className="h-4 w-4" />
         <Input
           value={search}
           onChange={(e) => setSearch(e.target.value)}
